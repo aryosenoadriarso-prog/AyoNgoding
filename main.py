30c02ced923b74b656eefe1e974d37aec94389c0
aryosenoadriarso-prog <aryosenoadriarso@gmail.com>
2026-01-06 13:13:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,6 @@ background = pygame.image.load("background.png").convert()
 background = pygame.transform.scale(background, (600, 400))
 
 
-def setDifficulty(value, difficulty):
-  print(f"Difficulty set to {difficulty}")
-
-
 def startGame():
   print("Game Started")
 
@@ -22,8 +18,6 @@ def startGame():
 menu = pygame_menu.Menu("Welcome", 400, 300, theme=themes.THEME_BLUE)
 
 menu.add.text_input("Name : ", default="Write Your Name")
-menu.add.selector("Difficulty : ", [("Hard", 1), ("Easy", 2)],
-                  onchange=setDifficulty)
 menu.add.button("Play", startGame)
 menu.add.button("Quit", pygame_menu.events.EXIT)
 
